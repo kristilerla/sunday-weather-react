39093a35a77336e10639b79c83ab4af46eca7f48
Kristil Erla Håland <78584348+kristilerla@users.noreply.github.com>
2024-05-26 15:26:36 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Weather from "./Weather";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h2>Hello World!!!</h2>
+      <Weather city="Paris" />
+      </header>
+     
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ src/Weather.js
@@ -0,0 +1,31 @@
+import React from "react";
+import axios from 'axios';
+import { Circles } from 'react-loader-spinner'
+;
+
+export default function Weather (props){
+   
+  
+    function handleResponse(response){
+alert (`the weather in ${response.data.name} is ${response.data.main.temp} °C`);
+
+    }
+   
+      
+
+
+    let apiKey = "04bde8cc7f569f7c5603cdbc6deb89a3";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return (<Circles
+        height="80"
+        width="80"
+        color="#fc03f8"
+        ariaLabel="circles-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+        />)
+};
+
+
