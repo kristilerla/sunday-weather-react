7387efb1e28d3ba87fd21ca2a9a97d6b58f86dce
Kristil Erla Håland <78584348+kristilerla@users.noreply.github.com>
2024-05-28 18:18:00 +0200

    upated devdependencies

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ export default function Footer()
 {
 return (
     <div>
-        Coded by <a>https://www.linkedin.com/in/kristilerla/</a> Kristil Erla and hosted on <a>https://github.com/kristilerla/sunday-weather-react</a>Github and <a>https://sundayweather.netlify.app/</a> Netlify
+        <h5>Coded by <a>https://www.linkedin.com/in/kristilerla/</a> Kristil Erla and hosted on <a>https://github.com/kristilerla/sunday-weather-react</a>Github and <a>https://sundayweather.netlify.app/</a> Netlify</h5>
     </div>
 );
 }
