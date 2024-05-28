cb6a2dd84ba5e5efcb20ff56e7a8414e36f6325b
Kristil Erla Håland <78584348+kristilerla@users.noreply.github.com>
2024-05-28 17:27:18 +0200

    changed background color

--- src/Weather.js
+++ src/Weather.js
@@ -3,20 +3,10 @@ import axios from 'axios';
 import { Hearts } from 'react-loader-spinner'
 ;
 
-export default function Weather (props){
+export default function Weather (){
    
-    function handleResponse(response){
-        return
-        <div > <h2> (`the weather in ${response.data.name} is ${response.data.main.temp} °C`);
-        </h2>
-        </div>
-            }
-        
-
-
-            let apiKey = "04bde8cc7f569f7c5603cdbc6deb89a3";
-            let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-            axios.get(apiUrl).then(handleResponse);
+   
+           
             return (<Hearts
                 height="80"
                 width="80"
