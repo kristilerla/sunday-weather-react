f63fb743a45e3f83fe125e0d52f170b1266bd8e1
Kristil Erla Håland <78584348+kristilerla@users.noreply.github.com>
2024-05-28 17:18:08 +0200

    updated with weathersearch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Weather from "./Weather";
+import WeatherSearch from "./WeatherSearch";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h2>Hello World!!!</h2>
-      <Weather city="Paris" />
+      <h2>Weather search engine</h2>
+      <Weather city="Paris"/>
+      <WeatherSearch city="London" />
       </header>
      
     </div>
@@ -15,3 +17,6 @@ function App() {
 }
 
 export default App;
+
+
+ 
--- src/Weather.js
+++ src/Weather.js
@@ -1,31 +1,32 @@
 import React from "react";
 import axios from 'axios';
-import { Circles } from 'react-loader-spinner'
+import { Hearts } from 'react-loader-spinner'
 ;
 
 export default function Weather (props){
    
-  
     function handleResponse(response){
-alert (`the weather in ${response.data.name} is ${response.data.main.temp} °C`);
+        return
+        <div > <h2> (`the weather in ${response.data.name} is ${response.data.main.temp} °C`);
+        </h2>
+        </div>
+            }
+        
 
-    }
-   
-      
 
-
-    let apiKey = "04bde8cc7f569f7c5603cdbc6deb89a3";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-    return (<Circles
-        height="80"
-        width="80"
-        color="#fc03f8"
-        ariaLabel="circles-loading"
-        wrapperStyle={{}}
-        wrapperClass=""
-        visible={true}
-        />)
+            let apiKey = "04bde8cc7f569f7c5603cdbc6deb89a3";
+            let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+            axios.get(apiUrl).then(handleResponse);
+            return (<Hearts
+                height="80"
+                width="80"
+                color="pink"
+                ariaLabel="hearts-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+                />)
+       
 };
-
-
+  
+  
